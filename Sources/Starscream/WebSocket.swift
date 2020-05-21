f201866552380bfb6c1872930eb3e114a2f61cad
Eugene Kwong <eugene.kwong@thescore.com>
2020-05-20 21:32:30 -0400

    Allow socks proxy to work for actual device

--- Sources/Starscream/WebSocket.swift
+++ Sources/Starscream/WebSocket.swift
@@ -157,8 +157,12 @@ open class FoundationStream : NSObject, WSStream, StreamDelegate  {
                 let proxyDict = CFNetworkCopySystemProxySettings()
                 let socksConfig = CFDictionaryCreateMutableCopy(nil, 0, proxyDict!.takeRetainedValue())
                 let propertyKey = CFStreamPropertyKey(rawValue: kCFStreamPropertySOCKSProxy)
-                CFWriteStreamSetProperty(outputStream, propertyKey, socksConfig)
-                CFReadStreamSetProperty(inputStream, propertyKey, socksConfig)
+                let dict = socksConfig as? [String: Any]
+                if let ip = dict?["HTTPSProxy"]  as? String, let port = dict?["HTTPSPort"] as? Int {
+                    let customSocksConfig = ["SOCKSProxy": ip, "SOCKSPort": port + 1, "SOCKSEnable": 1] as CFDictionary?
+                    CFWriteStreamSetProperty(outputStream, propertyKey, customSocksConfig)
+                    CFReadStreamSetProperty(inputStream, propertyKey, customSocksConfig)
+                }
             }
         #endif
         
